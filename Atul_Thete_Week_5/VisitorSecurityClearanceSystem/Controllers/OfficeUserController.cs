37a7272af15dd8ff0d9497a2b20f7648fbe01a00
Atul Thete <110735045+ATULTHETE27@users.noreply.github.com>
2024-06-06 22:04:44 +0530

    Add files via upload

---
+++ Atul_Thete_Week_5/VisitorSecurityClearanceSystem/Controllers/OfficeUserController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VisitorSecurityClearanceSystem.DTO;
+using VisitorSecurityClearanceSystem.Services;
+
+namespace VisitorSecurityClearanceSystem.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class OfficeUserController : Controller
+    {
+        private readonly IOfficeService _officeService;
+        private readonly IVisitorService _visitorService;
+
+
+        public OfficeUserController(IOfficeService officeService)
+        {
+            _officeService = officeService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(Login loginModel)
+        {
+            var officeUser = await _officeService.LoginOfficeUser(loginModel.Email, loginModel.Password);
+            if (officeUser == null)
+            {
+                return Unauthorized("Invalid credentials");
+            }
+
+            return Ok(officeUser);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Office> GetOfficeById(string id)
+        {
+            return await _officeService.GetOfficeById(id);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateOffice(string id, Office securityModel)
+        {
+            try
+            {
+                var updatedSecurity = await _officeService.UpdateOffice(id, securityModel);
+                return Ok(updatedSecurity);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error in UpdateVisitor (Controller): {ex.Message}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+        [HttpPut("{visitorId}")]
+        public async Task<IActionResult> UpdateVisitorStatus(string visitorId, bool newStatus)
+        {
+            try
+            {
+                var updatedVisitor = await _visitorService.UpdateVisitorStatus(visitorId, newStatus);
+                return Ok(updatedVisitor);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error in UpdateVisitorStatus (Controller): {ex.Message}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+        [HttpGet("{status}")]
+        public async Task<IActionResult> GetVisitorsByStatus(bool status)
+        {
+            var visitors = await _visitorService.GetVisitorsByStatus(status);
+            return Ok(visitors);
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOffice(string id)
+        {
+            await _officeService.DeleteOffice(id);
+            return NoContent();
+        }
+    }
+}
---
+++ Atul_Thete_Week_5/VisitorSecurityClearanceSystem/Controllers/VisitorController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VisitorSecurityClearanceSystem.DTO;
+using VisitorSecurityClearanceSystem.Services;
+
+namespace VisitorSecurityClearanceSystem.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class VisitorController : Controller
+    {
+        private readonly IVisitorService _visitorService;
+
+        public VisitorController(IVisitorService visitorService)
+        {
+            _visitorService = visitorService;
+        }
+
+        [HttpPost]
+        public async Task<Visitor> AddVisitor(Visitor visitorModel)
+        {
+            return await _visitorService.AddVisitor(visitorModel);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Visitor> GetVisitorById(string id)
+        {
+            return await _visitorService.GetVisitorById(id);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateVisitor(string id, Visitor visitorModel)
+        {
+            try
+            {
+                var updatedVisitor = await _visitorService.UpdateVisitor(id, visitorModel);
+                return Ok(updatedVisitor);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error in UpdateVisitor (Controller): {ex.Message}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVisitor(string id)
+        {
+            await _visitorService.DeleteVisitor(id);
+            return NoContent();
+        }
+
+    }
+}
