0fc221c5600689bc0c185f6c019f1c78784b3d52
Atul Thete <110735045+ATULTHETE27@users.noreply.github.com>
2024-06-26 09:43:14 +0530

    Add files via upload

---
+++ EmployeeManagementSystem/EmployeeManagementSystem/Program.cs
@@ -0,0 +1,33 @@
+using EmployeeManagementSystem.Common;
+using EmployeeManagementSystem.CosmoDB;
+using EmployeeManagementSystem.Entities;
+using EmployeeManagementSystem.Interface;
+using EmployeeManagementSystem.ServiceFilters;
+using EmployeeManagementSystem.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IEmployeeAdditionalDetailsService, EmployeeAdditionalDetailsService>();
+builder.Services.AddScoped<IEmployeeBasicDetailsService, EmployeeBasicDetailsService>();
+builder.Services.AddScoped<ICosmoDBService, CosmoDBService>();
+builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
+builder.Services.AddScoped<BuildEmployeeFilter>();
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ EmployeeManagementSystem/EmployeeManagementSystem/Services/EmployeeAdditionalDetailsService.cs
@@ -0,0 +1,75 @@
+﻿using AutoMapper;
+using EmployeeManagementSystem.CosmoDB;
+using EmployeeManagementSystem.DTO;
+using EmployeeManagementSystem.Entities;
+using EmployeeManagementSystem.Interface;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.Services
+{
+    public class EmployeeAdditionalDetailsService : IEmployeeAdditionalDetailsService
+    {
+        private readonly ICosmoDBService _cosmoDBService;
+        private readonly IMapper _autoMapper;
+        private readonly IEmployeeBasicDetailsService _basicDetailsService;
+
+        public EmployeeAdditionalDetailsService(ICosmoDBService cosmoDBService, IMapper mapper, IEmployeeBasicDetailsService basicDetailsService)
+        {
+            _cosmoDBService = cosmoDBService;
+            _autoMapper = mapper;
+            _basicDetailsService = basicDetailsService;
+        }
+
+        public async Task<EmployeeAdditionalDetailsDTO> GetEmployeeAdditionalDetailsById(string id)
+        {
+            var entity = await _cosmoDBService.GetEmployeeAdditionalDetailsById(id);
+            return _autoMapper.Map<EmployeeAdditionalDetailsDTO>(entity);
+        }
+
+        public async Task<EmployeeAdditionalDetailsDTO> AddEmployeeAdditionalDetails(EmployeeAdditionalDetailsDTO additionalDetailsDTO)
+        {
+            var basicDetails = await _basicDetailsService.GetEmployeeBasicDetailsById(additionalDetailsDTO.EmployeeBasicDetailsUId);
+
+            if (basicDetails == null)
+            {
+                throw new Exception("Basic details not found for the provided ID");
+            }
+
+            var entity = _autoMapper.Map<EmployeeAdditionalDetailsEntity>(additionalDetailsDTO);
+            var response = await _cosmoDBService.Add(entity);
+            return _autoMapper.Map<EmployeeAdditionalDetailsDTO>(response);
+        }
+
+
+
+        public async Task<EmployeeAdditionalDetailsDTO> UpdateEmployeeAdditionalDetails(string id, EmployeeAdditionalDetailsDTO additionalDetailsDTO)
+        {
+            var entity = await _cosmoDBService.GetEmployeeAdditionalDetailsById(id);
+            if (entity == null) throw new Exception("Employee not found");
+
+            _autoMapper.Map(additionalDetailsDTO, entity);
+            entity.Intialize(false, "employeeAdditionalDetails", "System", "System");
+
+            var response = await _cosmoDBService.Update(id, entity);
+            return _autoMapper.Map<EmployeeAdditionalDetailsDTO>(response);
+        }
+
+        public async Task DeleteEmployeeAdditionalDetails(string id)
+        {
+            var entity = await _cosmoDBService.GetEmployeeAdditionalDetailsById(id);
+            if (entity == null) throw new Exception("Employee not found");
+
+            await _cosmoDBService.DeleteAdditionalDetails(id);
+        }
+
+        public async Task<IEnumerable<EmployeeAdditionalDetailsDTO>> GetAllEmployeeAdditionalDetails()
+        {
+            var entities = await _cosmoDBService.GetAllAdditionalDetails();
+            return _autoMapper.Map<IEnumerable<EmployeeAdditionalDetailsDTO>>(entities);
+        }
+
+        
+    }
+}
