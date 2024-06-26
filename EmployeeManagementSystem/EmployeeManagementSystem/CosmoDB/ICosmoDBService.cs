17c326fd9a57fe9fc0cf373283e80ed05d309769
Atul Thete <110735045+ATULTHETE27@users.noreply.github.com>
2024-06-26 09:40:34 +0530

    Add files via upload

---
+++ EmployeeManagementSystem/EmployeeManagementSystem/CosmoDB/ICosmoDBService.cs
@@ -0,0 +1,20 @@
+﻿using EmployeeManagementSystem.Entities;
+
+namespace EmployeeManagementSystem.CosmoDB
+{
+    public interface ICosmoDBService
+    {
+        Task<T> Add<T>(T entity);
+        Task<T> Update<T>(string id,T entity);
+
+        Task DeleteAdditionalDetails(string id);
+        Task<EmployeeAdditionalDetailsEntity> GetEmployeeAdditionalDetailsById(string id);
+        Task<IEnumerable<EmployeeAdditionalDetailsEntity>> GetAllAdditionalDetails();
+
+
+        Task<EmployeeBasicDetailsEntity> GetEmployeeBasicDetailsById(string id);
+        Task DeleteBasicDetails(string id);
+        Task<IEnumerable<EmployeeBasicDetailsEntity>> GetAllBasicDetails();
+
+    }
+}
