64eb9e164e677dea0a394c9bf7f340b81e8fb153
Atul Thete <110735045+ATULTHETE27@users.noreply.github.com>
2024-07-10 17:21:20 +0530

    Add files via upload

---
+++ EmployeeManagementSystem/EmployeeManagementSystem25/Common/Credentials.cs
@@ -0,0 +1,16 @@
+﻿namespace EmployeeManagementSystem25.Common
+{
+    public class Credentials
+    {
+        public static readonly string DatabaseName = Environment.GetEnvironmentVariable("databaseName");
+        public static readonly string ContainerName = Environment.GetEnvironmentVariable("containerName");
+        public static readonly string CosmosEndpoint = Environment.GetEnvironmentVariable("cosmosUrl");
+        public static readonly string PrimaryKey = Environment.GetEnvironmentVariable("primaryKey");
+        public static readonly string EmployeeDataType = "Employee";
+        public static readonly string ManagerDatatype = "Manager";
+        public static readonly string AdditionalEmployee = "AdditionalEmployee";
+        internal static readonly string VisitorUrl = Environment.GetEnvironmentVariable("visitorUrl");
+        internal static readonly string AddVisitorEndPoint = "/api/Visitor/AddVisitor";
+        internal static readonly string GetAllVisitorEndPoint = "api/Visitor/GetAllVisitors";
+    }
+}
---
+++ EmployeeManagementSystem/EmployeeManagementSystem25/Services/EmployeeBasicDetailsService.cs
@@ -0,0 +1,138 @@
+﻿using AutoMapper;
+using EmployeeManagementSystem25.Common;
+using EmployeeManagementSystem25.CosmosDB;
+using EmployeeManagementSystem25.DTO;
+using EmployeeManagementSystem25.Entities;
+using EmployeeManagementSystem25.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos.Linq;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem25.Services
+{
+    public class EmployeeBasicDetailsService : IEmployeeBasicDetailsService
+    {
+        private readonly ICosmosDBService _cosmosDBService;
+        private readonly IMapper _mapper;
+
+        public EmployeeBasicDetailsService(ICosmosDBService cosmosDBService, IMapper mapper)
+        {
+            _cosmosDBService = cosmosDBService;
+            _mapper = mapper;
+        }
+
+        public async Task<EmployeeBasicDetailsEntity> AddEmployeeBasicDetails(EmployeeBasicDetailsEntity employeeEntity)
+        {
+            employeeEntity.Initialize(true, "Employee", "Atul", "Atul");
+            employeeEntity = await _cosmosDBService.AddEmployeeBasicDetails(employeeEntity);
+            return employeeEntity;
+        }
+
+
+
+        public async Task<List<EmployeeBasicDetailsEntity>> GetAllEmployeeBasicDetails()
+        {
+            return await _cosmosDBService.GetAllEmployeeBasicDetails();
+        }
+
+
+
+        public async Task<EmployeeBasicDetailsDTO> GetEmployeeBasicDetailsByUId(string UId)
+        {
+            var response = await _cosmosDBService.GetEmployeeBasicDetailsByUId(UId);
+            return _mapper.Map<EmployeeBasicDetailsDTO>(response);
+
+        }
+
+        public async Task<EmployeeBasicDetailsDTO> UpdateUserDetailsByUId(string UId, EmployeeBasicDetailsDTO employeeBasicDetailsDTO)
+        {
+            var existingUser = await _cosmosDBService.GetEmployeeBasicDetailsByUId(UId);
+            if (existingUser == null)
+            {
+                return null;
+            }
+
+            existingUser.Active = false;
+            existingUser.Archieved = true;
+
+            await _cosmosDBService.ReplaceAsync(UId, existingUser);
+
+            existingUser.Initialize(false, Credentials.EmployeeDataType, "Atul", "Atul");
+            _mapper.Map(employeeBasicDetailsDTO, existingUser);
+
+            existingUser = await _cosmosDBService.AddEmployeeBasicDetails(existingUser);
+            return _mapper.Map<EmployeeBasicDetailsDTO>(existingUser);
+        }
+
+        public async Task<EmployeeBasicDetailsDTO> DeleteUserByUId(string UId)
+        {
+            var existingUser = await _cosmosDBService.GetEmployeeBasicDetailsByUId(UId);
+            existingUser.Archieved = true;
+            await _cosmosDBService.ReplaceAsync(UId, existingUser);
+
+            existingUser.Initialize(false, Credentials.EmployeeDataType, "Atul", "Atul");
+            existingUser.Active = false;
+            existingUser.Archieved = true;
+            var response = await _cosmosDBService.AddEmployeeBasicDetails(existingUser);
+            return _mapper.Map<EmployeeBasicDetailsDTO>(response);
+
+        }
+
+        public async Task<EmployeeBasicDetailsDTO> AddEmployeeBasicDetails(EmployeeBasicDetailsDTO employee)
+        {
+            var entity = _mapper.Map<EmployeeBasicDetailsEntity>(employee);
+            entity.Initialize(true, "Employee", "Atul", "Atul");
+
+            var response = await _cosmosDBService.AddEmployeeBasicDetails(entity); // Await the async operation
+
+            return _mapper.Map<EmployeeBasicDetailsDTO>(response);
+        }
+
+        public async Task<List<EmployeeBasicDetailsEntity>> GetAllEmployeeBasicDetailsByRole(string role)
+        {
+            try
+            {
+                var allEmployees = await GetAllEmployeeBasicDetails();
+                return allEmployees.FindAll(a => a.Role == role);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception
+                throw new Exception("Error in GetAllEmployeeBasicDetailsByRole method", ex);
+            }
+        }
+
+        public async Task<EmployeeFilterCriteria> GetAllEmployeeBasicDetailsByPagination(EmployeeFilterCriteria employeeFilterCriteria)
+        {
+            EmployeeFilterCriteria criteria = new EmployeeFilterCriteria();
+
+            var employee = await GetAllEmployeeBasicDetails();
+            criteria.totalCount = employee.Count;
+
+            var skip = employeeFilterCriteria.PageSize * (employeeFilterCriteria.Page - 1);
+
+            employee = employee.Skip(skip).Take(employeeFilterCriteria.PageSize).ToList();
+
+            employeeFilterCriteria.Employee = employee;
+
+            return criteria;
+        }
+
+        public async Task<VisitorDTO> AddVisitorByMakePostRequest(VisitorDTO visitor)
+        {
+            var serialObj = JsonConvert.SerializeObject(visitor);
+            var requestObj = await HttpClientHelper.MakePostRequest(Credentials.VisitorUrl, Credentials.AddVisitorEndPoint, serialObj);
+            var responseObj = JsonConvert.DeserializeObject<VisitorDTO>(requestObj);
+            return responseObj;
+
+        }
+
+        public async Task<IEnumerable<VisitorDTO>> GetVisitorByMakePostRequest()
+        {
+            var responseString = await HttpClientHelper.MakeGetRequest(Credentials.VisitorUrl, Credentials.GetAllVisitorEndPoint);
+            var employees = JsonConvert.DeserializeObject<IEnumerable<VisitorDTO>>(responseString);
+            return employees;
+        }
+    }
+}
